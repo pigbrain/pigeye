fb88e977a215014c532b2575da6e1b9c13039489
pigbrain <pigbrain.lee@gmai.com>
2017-08-15 22:40:29 +0900

    Modify sql

--- table.sql
+++ table.sql
@@ -1,25 +1,28 @@
-CREATE TABLE `pigeye`.`service` (
-  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(30) NOT NULL,
-  `description` VARCHAR(50) NOT NULL,
-  `creation_datetime` DATETIME NOT NULL,
-  `updated_datetime` DATETIME NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `api` (
+  `api_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `service_id` bigint(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `url` varchar(1000) NOT NULL,
+  `user_agent` varchar(100) NOT NULL,
+  `content_type` varchar(100) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `request_body` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `response_body` text NOT NULL,
+  `success` tinyint(1) DEFAULT 0,
+  `notification_script` text,
+  `creation_datetime` datetime NOT NULL,
+  `updated_datetime` datetime NOT NULL,
+  PRIMARY KEY (`api_id`),
+  KEY `service_api_idx1` (`service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `pigeye`.`service_api` (
-  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-  `service_id` BIGINT(11) NOT NULL,
-  `name` VARCHAR(30) NOT NULL,
-  `description` VARCHAR(100) NOT NULL,
-  `url` VARCHAR(1000) NOT NULL,
-  `content_type` VARCHAR(500) NOT NULL,
-  `method` VARCHAR(10) NOT NULL,
-  `request_body` TEXT NOT NULL,
-  `status` INT NOT NULL,
-  `response_body` TEXT NOT NULL,
-  `api` TINYINT(1) NOT NULL,
-  `creation_datetime` DATETIME NOT NULL,
-  `updated_datetime` DATETIME NOT NULL,
-  PRIMARY KEY (id),
-  INDEX service_api_idx1 (service_id) );
+CREATE TABLE `service` (
+  `service_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `creation_datetime` datetime NOT NULL,
+  `updated_datetime` datetime NOT NULL,
+  PRIMARY KEY (`service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
